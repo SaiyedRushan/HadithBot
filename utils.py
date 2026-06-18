1a6530d3def4f31d5a217d9523f1d69775b5e3b8
Rushanshah Saiyed <rushan52@gmail.com>
2026-06-18 17:52:07 -0400

    remove old bot and references

--- utils.py
+++ utils.py
@@ -1,15 +1,7 @@
 from typing import Dict
 from dataclasses import dataclass
-import json
 import re
 
-@dataclass
-class Hadith:
-    hadith_no: int
-    english_narrator: str
-    english_text: str
-    chapters: dict[str, str]  # Contains 'english' and 'arabic' chapter names
-    books_metadata: dict[str, str]  # Contains 'english_title' and 'arabic_title'
 
 @dataclass
 class Name:
@@ -19,28 +11,12 @@ class Name:
     found: str
     en: Dict[str, str]
     fr: Dict[str, str]
-    
-@dataclass
-class Chapter:
-    english: str
-    arabic: str
-    id: int
-
-@dataclass
-class Book:
-    english_title: str
-    arabic_title: str
-    id: int
 
 
-def load_messages():
-    with open('hadiths.json', 'r', encoding='utf-8') as f:
-        return json.load(f)
-    
 def getHadithFormattedMessage(hadith) -> list[str]:
     formatted_messages = []
     formatted_messages.append(f"> ### Book: {hadith['books_metadata']['english_title']} - Chapter: {hadith['chapters']['english']}\n")
-    formatted_hadith = f"> #{hadith['id_in_book']} - {hadith['english_narrator']} {hadith['english_text']}\n\n" 
+    formatted_hadith = f"> #{hadith['id_in_book']} - {hadith['english_narrator']} {hadith['english_text']}\n\n"
     formatted_hadith = re.sub(r'\s+', ' ', formatted_hadith).strip()
 
     # while formatted_hadith is not empty
@@ -57,15 +33,13 @@ def getHadithFormattedMessage(hadith) -> list[str]:
 
     return formatted_messages
 
+
 def find_last_newline(message: str):
     last_newline = message.rfind('\n\n')
     if last_newline == -1:
         last_newline = message.rfind('.')
     return last_newline + 2
 
-def loadNames():
-    with open('99names.json', 'r', encoding='utf-8') as f:
-        return json.load(f)
 
 def getNameFormattedMessage(name) -> str:
     formatted_message = ""
